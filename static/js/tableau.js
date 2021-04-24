087acb210f87452a165aa1958edb505e42077c20
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-04-23 23:01:03 -0400

    updating tableau.js and index.html, adding first chart

---
+++ static/js/tableau.js
@@ -0,0 +1,17 @@
+(function ufcViz() {
+
+    let ufcContainer = document.getElementById("fighterWeight");
+    let ufcURL = "https://public.tableau.com/views/Project-3_16186819550490/WeightClass?:language=en&:display_count=y&:origin=viz_share_link";
+
+
+
+
+
+
+
+
+
+
+
+
+})()
