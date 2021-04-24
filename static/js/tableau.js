9e2a5a13c7650e36e58b1cc0740a73b64feb1f60
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-04-23 23:12:00 -0400

    added additional chart to tableau.js

--- static/js/tableau.js
+++ static/js/tableau.js
@@ -1,7 +1,15 @@
 (function ufcViz() {
 
-    let ufcContainer = document.getElementById("fighterWeight");
+    let ufcWeightContainer = document.getElementById("fighterWeight");
     let ufcURL = "https://public.tableau.com/views/Project-3_16186819550490/WeightClass?:language=en&:display_count=y&:origin=viz_share_link";
+
+    let ufcCountryContainer = document.getElementById("fightCountry");
+    let ufcCountryURL = "https://public.tableau.com/views/Project-3_16186819550490/Sheet1?:language=en&:display_count=y&:origin=viz_share_link";
+
+
+
+    new tableau.Viz(ufcWeightContainer, ufcURL);
+    new tableau.Viz(ufcCountryContainer, ufcCountryURL);
 
 
 
