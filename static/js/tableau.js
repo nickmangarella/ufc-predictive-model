93b7a4e3ff95d4f2c3758c5568dfb632f4a6edea
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-04-23 23:18:58 -0400

    adding chart to tableau.js and index.html

--- static/js/tableau.js
+++ static/js/tableau.js
@@ -1,15 +1,19 @@
 (function ufcViz() {
 
-    let ufcWeightContainer = document.getElementById("fighterWeight");
+    let ufcWeight = document.getElementById("fighterWeight");
     let ufcURL = "https://public.tableau.com/views/Project-3_16186819550490/WeightClass?:language=en&:display_count=y&:origin=viz_share_link";
 
-    let ufcCountryContainer = document.getElementById("fightCountry");
+    let ufcCountry = document.getElementById("fightCountry");
     let ufcCountryURL = "https://public.tableau.com/views/Project-3_16186819550490/Sheet1?:language=en&:display_count=y&:origin=viz_share_link";
 
+    let ufcCountryRank = document.getElementById("fightCountryRank");
+    let ufcCountryRankURL = "https://public.tableau.com/views/Project-3_16186819550490/CountrytopRanks?:language=en&:display_count=y&:origin=viz_share_link";
 
 
-    new tableau.Viz(ufcWeightContainer, ufcURL);
-    new tableau.Viz(ufcCountryContainer, ufcCountryURL);
+
+    new tableau.Viz(ufcWeight, ufcURL);
+    new tableau.Viz(ufcCountry, ufcCountryURL);
+    new tableau.Viz(ufcCountryRank, ufcCountryRankURL);
 
 
 
